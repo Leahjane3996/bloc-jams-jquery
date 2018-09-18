96279166dc192a04a8080bf92f62ee7ecd98c0b4
Leah <ljoberh@gmail.com>
2018-09-18 17:01:40 -0600

    updates

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,7 +1,11 @@
+$( document ).ready(function () {
+  // Handler for.ready () called.
+});
+
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
   $('#artist').text(album.artist);
-  $('#release-info').text(album.release-info);
+  $('#release-info').text(album.releaseInfo);
 
 }
